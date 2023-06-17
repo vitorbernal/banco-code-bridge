4272128fd24c2a1bf02439ab631e8a457b3578f6
Vitor Bernal <vitor.bernal159@gmail.com>
2023-06-17 13:07:02 -0300

    novos dados

--- src/clientes/models.py
+++ src/clientes/models.py
@@ -5,8 +5,8 @@ class Clientes(models.Model):
     ESTADOS = [
         ("SP", "São Paulo"),
         ("MG", "Minas Gerais"),
-        ("GO", "Goiás")
-        ("TO", "Tocantins")
+        ("GO", "Goiás"),
+        ("TO", "Tocantins"),
     ]
 
     nome = models.CharField(max_length=100, null=False, blank=False)
--- src/contas/models.py
+++ src/contas/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Conta(models.Model):
-    cliente = models.ForeignKey('clientes.Clientes', null=True, on_delete=models.SET_NULL)
+    cliente = models.ForeignKey('clientes.Clientes', null=True, on_delete=models.SET_NULL, related_name='contas')
     saldo = models.DecimalField('saldo do cliente', max_digits=12, decimal_places=2, default=0)
     data_abertura = models.DateTimeField(auto_now_add=True, null=False)
+    
+    def depositar(self,valor):
+        self.saldo += valor
+        self.save()
+
+    def debitar(self,valor):
+        self.saldo -= valor
+        self.save()    
 
     def __str__(self):
-        return f'{self.cliente} - R${self.saldo}'
+        return f'{self.cliente} - R${self.saldo}'   
